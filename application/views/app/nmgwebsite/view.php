2a85e5a63946f4c59cf55f8b95994687964a6eb6
Reymond Samontena <reymond@nmgresources.ph>
2018-07-17 17:38:55 +0800

    JS files, changes on view.php

--- application/views/app/nmgwebsite/view.php
+++ application/views/app/nmgwebsite/view.php
@@ -9,6 +9,9 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/style.css")?>">
 	<script src="<?php echo base_url("assets/js/bootstrap.js")?>"></script>
 	<script src="<?php echo base_url("assets/js/bootstrap.min.js")?>"></script>
+	<script src="<?php echo base_url("assets/js/bootstrap.min4.js")?>"></script>
+	<script src="<?php echo base_url("assets/js/jquery.slim.min.js")?>"></script>
+    <script src="<?php echo base_url("assets/js/popper.min.js")?>"></script>
 </head>
 
 <header>
@@ -40,9 +43,6 @@
    			 </div>
 
   </nav>
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
          
 </div>
 			<div class="container">
