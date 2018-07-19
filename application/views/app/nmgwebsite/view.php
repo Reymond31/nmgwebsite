430ef14774272af61eed57d8396672b3e7ba64cc
Reymond Samontena <reymond@nmgresources.ph>
2018-07-19 13:51:07 +0800

    homepage

--- application/views/app/nmgwebsite/view.php
+++ application/views/app/nmgwebsite/view.php
@@ -4,6 +4,8 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/hover.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/hover.min.css")?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap.css")?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap.min.css")?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/style.css")?>">
@@ -11,160 +13,138 @@
 	<script src="<?php echo base_url("assets/js/bootstrap.min.js")?>"></script>
 	<script src="<?php echo base_url("assets/js/bootstrap.min4.js")?>"></script>
 	<script src="<?php echo base_url("assets/js/jquery.slim.min.js")?>"></script>
-    <script src="<?php echo base_url("assets/js/popper.min.js")?>"></script>
+	<script src="<?php echo base_url("assets/js/popper.min.js")?>"></script>
 </head>
-
-<header>
-	<div class="one">
-	<div class="container-fluid">
-        <nav class ="navbar navbar-default navbar-fixed-top" role= "navigation">
-           <div class = "container ">
-              <div class ="navbar-header">
-             	<img src="<?php echo base_url("assets/img/logo.png")?>" id="damn" style="height:65px">
-	               <button type ="button" class="navbar-toggle " data-toggle="collapse" data-target="#nav-collapse">
-		               <span class="icon-bar" ></span>
-		               <span class="icon-bar"></span>
-		               <span class="icon-bar"></span>  
-	         	  </button>
-       			</div>
-       				<div class="collapse navbar-collapse" id="nav-collapse" style="float:right">
-
-				           <ul class="nav navbar-nav">
-				             <li><a href="#uno"> ABOUT US</a></li>
-				          
-				             <li><a href="#tres">SERVICES</a></li>
-				          
-				             <li><a href="#">PRODUCTS</a></li>
-				             
-				             <li><a href="#sinko">CONTACT US</a></li>
-				          </ul>
-
-        			</div>
-   			 </div>
-
-  </nav>
-         
-</div>
-			<div class="container">
-				<div class="typewriter">
-				<div class="row">
-					<div class="col-lg-4 col-xs-12">
-						<p id="mission">OUR MISSION: </p>
-					</div>
-				</div>
-				<div class="row">
-						<div class="col-lg-6 col-xs-12">
-							<p id="our"><b>THIS IS A </b></p>
-					
-						</div>
-					</div>
-				
-					<div class="row">
-						<div class="col-lg-8 col-xs-12">
-							<p id="ours"><b>SAMPLE MISSION.</b></p>
-					
-						</div>
-					</div>
-					<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: white; width: 30%; float: left; margin-right: 1%;">
-					<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: white; width: 7%; float: left; margin-right: 1%;">
-					<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: white; width: 2%; float: left; margin-right: 1%;">
-				</div>
-			</div>
-</div>
-
-</header>
 <body>
-	<div id="uno">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-5">
-				<p class="big">A Web Development Company </p>
-				
+<div id="landingpage">
+	<div class="overlay">
+	<div class="container-fluid">
+		<nav class ="navbar navbar-default navbar-fixed-top" role= "navigation">
+			<div class="container">
+				<div class ="navbar-header">
+					<img src="<?php echo base_url("assets/img/logo.png")?>" class="logo">
+				</div>
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="#aboutus" class="hvr-bounce-in"> ABOUT US</a></li>
+						<li><a href="#services" class="hvr-bounce-in">SERVICES</a></li>
+						<li><a href="#products" class="hvr-bounce-in">PRODUCTS</a></li>
+						<li><a href="#contactus" class="hvr-bounce-in">CONTACT US</a></li>
+					</ul>
+				</div>
 			</div>
-			<div class="col-lg-7">
-				<p class="small"> NMG's main goal is to promote interactivity, robust functionality as well as user-friendliness through its products and services that meet and satiate every client's expectations accordingly, with full accountability sans errs and delays. We seriously believe in such proverbial adage that the customer is always right. That's why the company puts the clients at the top of its list, as it aims to meet the clients' utmost standards, helping them facilitate and develop their businesses through its maximized efforts.</p>
-				<button type="button" class="btnz btn-primary btn-login"> READ MORE</button>
-			</div>
-			</div>
-			<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: #0099ff;  width: 25%; float: left; margin-right: 2%;">
-				<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: #0099ff;  width: 15%; float: left; margin-right: 2%;">
-				<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: #0099ff;  width: 5%; float: left; margin-right: 2%;">
-			<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: #0099ff;  width: 25%; float: left; margin-right: 2%;">
-				<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: #0099ff;  width: 15%; float: left; margin-right: 2%;">
-				<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: #0099ff;  width: 3%; float: left; margin-right: 2%;">
-
-
+		</nav>
 	</div>
-</div>
-
-<div id="tres">
-	<div class="container">
-		<p id="services"> OUR SERVICES</p>
-		<div class="row"><center>
-			<div class="col-lg-4">
-				<img src="<?php echo base_url("assets/img/gallery.png")?>" class="img-responsive" id="gallery">
-				<h3 id="hey">Web Based Database Programming</h3>
-				<h5>Simple to complex data structuring, processing and database optimization</h5>
-		 	</div>
-			<div class="col-lg-4">
-		 		<img src="<?php echo base_url("assets/img/pen.png")?>" class="img-responsive" id="pen">
-		 		<h3>Website Design</h3>
-		 		<h5>Art and creative direction, graphic and user interface design for individual and corporate website </h5>
-
-		 	</div>
-			<div class="col-lg-4">
-		 		<img src="<?php echo base_url("assets/img/monitoe.png")?>" class="img-responsive" id="monitoe">
-		 		<h3>Website Development</h3>
-		 		<h5>Comprehensive interactive components programming and web scripting services.</h5></center>
-		 	</div>
-		 </div>
 	</div>
-</div>
-<div id="kwatro">
-
-		<div class="row">
-		<div class="container">
-			<div class="col-lg-3">
-			<p id="general"> HAVE A QUESTION FOR US?</p>
-				<button type="button" class="btnc btn-primary btn-login">CONTACT US</button>
+	<div class="container">
+		<div class="welcome-message">
+			<div class="row">
+				<div class="col-lg-12 col-xs-12">
+					<p>NMG Resources Inc. </p>
+				</div>
 			</div>
-			<div class="col-lg-3">
-			<p id="general"> WANT TO WORK FOR US?</p>
-				<button type="button" class="btnc btn-primary btn-login">YOU BETCHA</button>
-			</div>
-			<div class="col-lg-3">
-			<p id="general"> HAVE A PROJECT FOR US?</p>
-				<button type="button" class="btnc btn-primary btn-login">TELL US MORE</button>
-			</div>
-			<div class="col-lg-3">
-			<p id="general"> THINGS WONKY ON YOUR SITE?</p>
-				<button type="button" class="btnc btn-primary btn-login">ACTIVATE THE BAT SIGNAL</button>
+			<div class="row">
+				<div class="col-lg-12 col-xs-12">
+					<p><b>Expertise on Demand</b></p>
+				</div>
 			</div>
 		</div>
 	</div>
-	
 </div>
-</div>
-<div id="sinko">
-		<p id="contacts"> CONTACT US </p>
+
+
+
+	<div id="uno">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-5">
+					<p class="big">A Web Development Company </p>
+
+				</div>
+				<div class="col-lg-7">
+					<p class="small"> NMG's main goal is to promote interactivity, robust functionality as well as user-friendliness through its products and services that meet and satiate every client's expectations accordingly, with full accountability sans errs and delays. We seriously believe in such proverbial adage that the customer is always right. That's why the company puts the clients at the top of its list, as it aims to meet the clients' utmost standards, helping them facilitate and develop their businesses through its maximized efforts.</p>
+					<button type="button" class="btnz btn-primary btn-login"> READ MORE</button>
+				</div>
+			</div>
+			<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: #0099ff;  width: 25%; float: left; margin-right: 2%;">
+			<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: #0099ff;  width: 15%; float: left; margin-right: 2%;">
+			<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: #0099ff;  width: 5%; float: left; margin-right: 2%;">
+			<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: #0099ff;  width: 25%; float: left; margin-right: 2%;">
+			<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: #0099ff;  width: 15%; float: left; margin-right: 2%;">
+			<hr style="color: white; margin-bottom: 0px; height: 8px; background-color: #0099ff;  width: 3%; float: left; margin-right: 2%;">
+
+
+		</div>
+	</div>
+
+	<div id="tres">
+		<div class="container">
+			<p id="services"> OUR SERVICES</p>
+			<div class="row"><center>
+				<div class="col-lg-4">
+					<img src="<?php echo base_url("assets/img/gallery.png")?>" class="img-responsive" id="gallery">
+					<h3 id="hey">Web Based Database Programming</h3>
+					<h5>Simple to complex data structuring, processing and database optimization</h5>
+				</div>
+				<div class="col-lg-4">
+					<img src="<?php echo base_url("assets/img/pen.png")?>" class="img-responsive" id="pen">
+					<h3>Website Design</h3>
+					<h5>Art and creative direction, graphic and user interface design for individual and corporate website </h5>
+
+				</div>
+				<div class="col-lg-4">
+					<img src="<?php echo base_url("assets/img/monitoe.png")?>" class="img-responsive" id="monitoe">
+					<h3>Website Development</h3>
+					<h5>Comprehensive interactive components programming and web scripting services.</h5></center>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="kwatro">
 
 		<div class="row">
 			<div class="container">
+				<div class="col-lg-3">
+					<p id="general"> HAVE A QUESTION FOR US?</p>
+					<button type="button" class="btnc btn-primary btn-login">CONTACT US</button>
+				</div>
+				<div class="col-lg-3">
+					<p id="general"> WANT TO WORK FOR US?</p>
+					<button type="button" class="btnc btn-primary btn-login">YOU BETCHA</button>
+				</div>
+				<div class="col-lg-3">
+					<p id="general"> HAVE A PROJECT FOR US?</p>
+					<button type="button" class="btnc btn-primary btn-login">TELL US MORE</button>
+				</div>
+				<div class="col-lg-3">
+					<p id="general"> THINGS WONKY ON YOUR SITE?</p>
+					<button type="button" class="btnc btn-primary btn-login">ACTIVATE THE BAT SIGNAL</button>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
+<div id="sinko">
+	<p id="contacts"> CONTACT US </p>
+
+	<div class="row">
+		<div class="container">
 			<div class="col-lg-4"><center>
 				<img src="<?php echo base_url("assets/img/add.jpg")?>" class="img-responsive" id="gallerys">
 				<h5>136 Yakal St., <br>Makati City, 1203 Kalakhang Maynila</h5> 
-		 	</div>
+			</div>
 			<div class="col-lg-4">
-		 		<img src="<?php echo base_url("assets/img/call.png")?>" class="img-responsive" id="pens">
-		 		<h5> (02) 576-6505</h5>
-		 	</div>
+				<img src="<?php echo base_url("assets/img/call.png")?>" class="img-responsive" id="pens">
+				<h5> (02) 576-6505</h5>
+			</div>
 			<div class="col-lg-4">
-		 		<img src="<?php echo base_url("assets/img/email.png")?>" class="img-responsive" id="monitoes">
-		 		<h5>info@nmgresources.ph </h5>
-		 		</center>
-		 	</div>
-		 </div>
-	</div></div>
+				<img src="<?php echo base_url("assets/img/email.png")?>" class="img-responsive" id="monitoes">
+				<h5>info@nmgresources.ph </h5>
+			</center>
+		</div>
+	</div>
+</div></div>
 
 	<!--	<div style="float:right;" class="col-lg-6">
 			<input type="text" value="NAME" class="name">
@@ -182,15 +162,15 @@
 		      <button type="button" class="btna btn-primary btn-login"><b>SUBMIT</b></button>
 				
 
-		</div> -->
-	
-<center>
-		
-	<div class="mapouter"><div class="gmap_canvas"><iframe width="1334" height="585" id="gmap_canvas" src="https://maps.google.com/maps?q=136%20yakal%20st.%20makati%2Cphilippines&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.crocothemes.net"></a></div><style>.mapouter{text-align:right;height:585px;width:1334px;}.gmap_canvas {overflow:hidden;background:none!important;height:585px;width:1334px;}</style><small><a href="http://www.elegantthemes.com/affiliates/idevaffiliate.php?id=28532&url=42158&tid1=google3" rel="nofollow"></a></small></div></center>
-</div>
+		  </div> -->
 
-</div>
-</div>
+		  <center>
+
+		  	<div class="mapouter"><div class="gmap_canvas"><iframe width="1334" height="585" id="gmap_canvas" src="https://maps.google.com/maps?q=136%20yakal%20st.%20makati%2Cphilippines&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.crocothemes.net"></a></div><style>.mapouter{text-align:right;height:585px;width:1334px;}.gmap_canvas {overflow:hidden;background:none!important;height:585px;width:1334px;}</style><small><a href="http://www.elegantthemes.com/affiliates/idevaffiliate.php?id=28532&url=42158&tid1=google3" rel="nofollow"></a></small></div></center>
+		  </div>
+
+		</div>
+	</div>
 </center></div>
 </div>
 
@@ -201,6 +181,5 @@
 
 <script type="text/javascript">$(window).scroll(function(){$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
 });</script>
-</html>
-
+</body>
 </html>
