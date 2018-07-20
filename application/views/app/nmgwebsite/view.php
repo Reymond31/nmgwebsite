35d8cc8141cf0cadebec347c3e37e1c8616f9af1
Reymond Samontena <reymond@nmgresources.ph>
2018-07-20 16:13:15 +0800

    so many changes

--- application/views/app/nmgwebsite/view.php
+++ application/views/app/nmgwebsite/view.php
@@ -4,11 +4,18 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" media="all">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/aos.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/fontawesome.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/fontawesome.min.css")?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/hover.css")?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/hover.min.css")?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap.css")?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap.min.css")?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/style.css")?>">
+	<script src="<?php echo base_url("assets/js/aos.js")?>"></script>
+	<script src="<?php echo base_url("assets/js/fontawesome.js")?>"></script>
+	<script src="<?php echo base_url("assets/js/fontawesome.min.js")?>"></script>
 	<script src="<?php echo base_url("assets/js/bootstrap.js")?>"></script>
 	<script src="<?php echo base_url("assets/js/bootstrap.min.js")?>"></script>
 	<script src="<?php echo base_url("assets/js/bootstrap.min4.js")?>"></script>
@@ -65,6 +72,11 @@
             			</div>
             		</div>
 				</div>
+				<div class="arrow-down">
+					<a href="#aboutus" class="hvr-icon-down">
+  						<i class="fa fa-angle-double-down fa-2x hvr-icon-down:hover hvr-icon hvr-icon-down:focus hvr-icon,hvr-icon-down:active hvr-icon"></i>
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -73,7 +85,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-5">
-				<p class="big">We Are NMG</p></div>
+				<p class="big" data-aos="flip-down" data-aos-offset="200" data-aos-duration="2000">We Are <br> NMG</p></div>
 			<div class="col-lg-7">
 				<p class="small"> NMG's main goal is to promote interactivity, robust functionality as well as user-friendliness through its products and services that meet and satiate every client's expectations accordingly, with full accountability sans errs and delays. We seriously believe in such proverbial adage that the customer is always right. That's why the company puts the clients at the top of its list, as it aims to meet the clients' utmost standards, helping them facilitate and develop their businesses through its maximized efforts.</p>
 				<button type="button" class="btnz btn-primary btn-login"> READ MORE</button>
@@ -181,7 +193,7 @@
 
 <script>
 	$(window).scroll(function(){
-		$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+		$('nav').toggleClass('scrolled', $(this).scrollTop() > 500);
 	});
 	$(document).ready(function() {
     $('#firstSlide').fadeIn();
@@ -197,6 +209,7 @@
         });
     },1000);  
 });
+	AOS.init();
 </script>
 </body>
 </html>
