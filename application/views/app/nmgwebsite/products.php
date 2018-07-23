390f92744741023b12fa7c10354736f33b112b80
Reymond Samontena <reymond@nmgresources.ph>
2018-07-23 19:28:17 +0800

    Services

--- application/views/app/nmgwebsite/products.php
+++ application/views/app/nmgwebsite/products.php
@@ -21,7 +21,14 @@
 		</div>
 		<div class="row">
 			<div class="container">
-				<div class="col-lg-12 col-xs-12">
+				<div class="col-lg-6 col-xs-6">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<img src="<?php echo base_url("assets/img/txtsupport.jpg")?>" class="txtsupport">
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6 col-xs-6">
 					<div class="panel panel-default">
 						<div class="panel-body">
 							<img src="<?php echo base_url("assets/img/txtsupport.jpg")?>" class="txtsupport">
@@ -32,7 +39,14 @@
 		</div>
 		<div class="row">
 			<div class="container">
-				<div class="col-lg-12 col-xs-12">
+				<div class="col-lg-6 col-xs-6">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<img src="<?php echo base_url("assets/img/bes.jpg")?>" class="bes">
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6 col-xs-6">
 					<div class="panel panel-default">
 						<div class="panel-body">
 							<img src="<?php echo base_url("assets/img/bes.jpg")?>" class="bes">
@@ -43,7 +57,14 @@
 		</div>
 		<div class="row">
 			<div class="container">
-				<div class="col-lg-12 col-xs-12">
+				<div class="col-lg-6 col-xs-6">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<img src="<?php echo base_url("assets/img/cress.jpg")?>" class="cress">
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6 col-xs-6">
 					<div class="panel panel-default">
 						<div class="panel-body">
 							<img src="<?php echo base_url("assets/img/cress.jpg")?>" class="cress">
@@ -54,7 +75,14 @@
 		</div>
 		<div class="row">
 			<div class="container">
-				<div class="col-lg-12 col-xs-12">
+				<div class="col-lg-6 col-xs-6">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<img src="<?php echo base_url("assets/img/pinoypayroll.jpg")?>" class="pinoypayroll">
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6 col-xs-6">
 					<div class="panel panel-default">
 						<div class="panel-body">
 							<img src="<?php echo base_url("assets/img/pinoypayroll.jpg")?>" class="pinoypayroll">
