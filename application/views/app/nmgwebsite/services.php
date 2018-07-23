52c69e97279e6d5bd1899e8f54add64844633bbf
Ara <ara@nmgresources.ph>
2018-07-23 16:33:39 +0800

    change

--- application/views/app/nmgwebsite/services.php
+++ application/views/app/nmgwebsite/services.php
@@ -6,20 +6,20 @@
 			</div>
 			<div class="col-lg-6">
 				<p class="head">WHAT WE OFFER?</p>
-				<span>Desktop Application Development</span>
-				<p class="exp">Custom application development using Java / .Net. Applications range from client-server monitoring systems to full CRM suites.</p>
+				<span class="a">Desktop Application Development</span>
+				<p class="expa">Custom application development using Java / .Net. Applications range from client-server monitoring systems to full CRM suites.</p>
 
-				<span>Web Application Design and Development</span>	
-				<p class="exp">PHP / nodeJS custom development, including relevant database solutions. Applications are built using modern frameworks and industry leading scalability.</p>
+				<span class="a">Web Application Design and Development</span>	
+				<p class="expa">PHP / nodeJS custom development, including relevant database solutions. Applications are built using modern frameworks and industry leading scalability.</p>
 
-				<span>Mobile Application for Android and iOS</span>
+				<span class="a">Mobile Application for Android and iOS</span>
 				<p class="exp">Designing and development of both native and cross platform mobile applications. Applications backends are also designed with web-based management portals aligned to customer requirements.</p>
  				
- 				<span>Staff Augmentation</span>
- 				<p class="exp">Providing experienced developers and database administrators to local industries, managed for specific requirements of the business.</p>
+ 				<span class="a">Staff Augmentation</span>
+ 				<p class="expa">Providing experienced developers and database administrators to local industries, managed for specific requirements of the business.</p>
 
- 				<span>Technology & Management Consulting</span>
- 				<p class="exp">Specialist consulting services focused on requirements analysis, system design and project management, delivered to enterprise-level clients.</p>
+ 				<span class="a">Technology & Management Consulting</span>
+ 				<p class="expa">Specialist consulting services focused on requirements analysis, system design and project management, delivered to enterprise-level clients.</p>
 			</div>
 		</div>
 	</div>
