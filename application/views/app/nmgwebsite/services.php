a2d68f4681915f0cb054824ed38834edda8be8a4
Ara <ara@nmgresources.ph>
2018-07-23 16:11:37 +0800

    SERVICES

--- application/views/app/nmgwebsite/services.php
+++ application/views/app/nmgwebsite/services.php
@@ -1,21 +1,25 @@
-<div id="services">
-	<div class="container">
-		<p>OUR SERVICES</p>
-		<div class="row"><center>
-			<div class="col-lg-4">
-				<img src="<?php echo base_url("assets/img/gallery.png")?>" class="img-responsive" id="gallery">
-				
-				<h5>Simple to complex data structuring, processing and database optimization</h5>
+<div id="service" style="background:url(assets/img/two.jpg);background-attachment: fixed; background-position: center;background-repeat: no-repeat;background-size: cover;" >
+	<div class="row">
+		<div class="container">
+			<div class="col-lg-6">
+				<img src="assets/img/services3.jpg" class="laptop">
 			</div>
-			<div class="col-lg-4">
-				<img src="<?php echo base_url("assets/img/pen.png")?>" class="img-responsive" id="pen">
-				<h3>Website Design</h3>
-				<h5>Art and creative direction, graphic and user interface design for individual and corporate website </h5>
-			</div>
-			<div class="col-lg-4">
-				<img src="<?php echo base_url("assets/img/monitoe.png")?>" class="img-responsive" id="monitoe">
-				
-				<h5>Comprehensive interactive components programming and web scripting services.</h5></center>
+			<div class="col-lg-6">
+				<p class="head">WHAT WE OFFER?</p>
+				<span>Desktop Application Development</span>
+				<p class="exp">Custom application development using Java / .Net. Applications range from client-server monitoring systems to full CRM suites.</p>
+
+				<span>Web Application Design and Development</span>	
+				<p class="exp">PHP / nodeJS custom development, including relevant database solutions. Applications are built using modern frameworks and industry leading scalability.</p>
+
+				<span>Mobile Application for Android and iOS</span>
+				<p class="exp">Designing and development of both native and cross platform mobile applications. Applications backends are also designed with web-based management portals aligned to customer requirements.</p>
+ 				
+ 				<span>Staff Augmentation</span>
+ 				<p class="exp">Providing experienced developers and database administrators to local industries, managed for specific requirements of the business.</p>
+
+ 				<span>Technology & Management Consulting</span>
+ 				<p class="exp">Specialist consulting services focused on requirements analysis, system design and project management, delivered to enterprise-level clients.</p>
 			</div>
 		</div>
 	</div>
