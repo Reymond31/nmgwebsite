40855e8eb13aa3fc8501919dfb4e0a749492edfb
Reymond Samontena <reymond@nmgresources.ph>
2018-07-23 16:41:28 +0800

    changes in services

--- application/views/app/nmgwebsite/products.php
+++ application/views/app/nmgwebsite/products.php
@@ -17,7 +17,7 @@
 				<div class="col-lg-12 col-xs-12">
 					<div class="panel panel-default">
 						<div class="panel-body">
-							Basic panel example
+							<img src="<?php echo base_url("assets/img/txtsupport.jpg")?>" class="txtsupport">
 						</div>
 					</div>
 				</div>
@@ -28,7 +28,7 @@
 				<div class="col-lg-12 col-xs-12">
 					<div class="panel panel-default">
 						<div class="panel-body">
-							Basic panel example
+							<img src="<?php echo base_url("assets/img/bes.jpg")?>" class="bes">
 						</div>
 					</div>
 				</div>
@@ -39,7 +39,7 @@
 				<div class="col-lg-12 col-xs-12">
 					<div class="panel panel-default">
 						<div class="panel-body">
-							Basic panel example
+							<img src="<?php echo base_url("assets/img/cress.jpg")?>" class="cress">
 						</div>
 					</div>
 				</div>
@@ -50,7 +50,7 @@
 				<div class="col-lg-12 col-xs-12">
 					<div class="panel panel-default">
 						<div class="panel-body">
-							Basic panel example
+							<img src="<?php echo base_url("assets/img/pinoypayroll.jpg")?>" class="pinoypayroll">
 						</div>
 					</div>
 				</div>
