ec023cf2422d325061c623acecc372c9a8f9523e
Reymond Samontena <reymond@nmgresources.ph>
2018-07-23 17:43:17 +0800

    products

--- application/views/app/nmgwebsite/products.php
+++ application/views/app/nmgwebsite/products.php
@@ -2,8 +2,15 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="container">
-				<div class="col-lg-12 col-xs-12">
-					<h1>PRODUCTS</h1>
+				<h1>PRODUCTS</h1>
+				<div class="col-lg-6 col-xs-6">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<img src="<?php echo base_url("assets/img/heffy.jpg")?>" class="heffy">
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6 col-xs-6">
 					<div class="panel panel-default">
 						<div class="panel-body">
 							<img src="<?php echo base_url("assets/img/heffy.jpg")?>" class="heffy">
