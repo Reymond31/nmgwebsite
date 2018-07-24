9a592ca310b35ed3d22c062b16da83505f270bd0
Mae0201 <mae@nmgresources.ph>
2018-07-24 19:06:30 +0800

    contact us

--- application/views/app/nmgwebsite/contactus.php
+++ application/views/app/nmgwebsite/contactus.php
@@ -1,45 +1,42 @@
 <div id="contactus">
-	<p id="contacts"> CONTACT US </p>
-
 	<div class="row">
 		<div class="container">
-			<div class="col-lg-4"><center>
-				<img src="<?php echo base_url("assets/img/add.jpg")?>" class="img-responsive" id="gallerys">
-				<h5>136 Yakal St., <br>Makati City, 1203 Kalakhang Maynila</h5> 
-			</div>
-			<div class="col-lg-4">
-				<img src="<?php echo base_url("assets/img/call.png")?>" class="img-responsive" id="pens">
-				<h5> (02) 576-6505</h5>
-			</div>
-			<div class="col-lg-4">
-				<img src="<?php echo base_url("assets/img/email.png")?>" class="img-responsive" id="monitoes">
-				<h5>info@nmgresources.ph </h5>
-			</center>
-			</div>
-	</div>
-</div></div>
-
-	<div style="float:left;" class="col-lg-6">
-			<input type="text" value="NAME" class="name">
-			<input type="email" value="EMAIL" class="email">
-			<select>
-				<option>Reason for Contacting NMG Resources</option>
+			
+			<h1> CONTACT US </h1>
+			<div class="col-lg-7">
+				<h2> Have a general question?</h2>
+				Fill up the form:<br>
+			
+			Name:
+			<br>
+			<input type="text" value="Your Name..." class="name"><br>
+			
+			Email:
+			<br>
+			<input type="email" value="Your Email..." class="email"><br>
+			<select class="option">
+				<option>Purpose</option>
 					<option> General Inquiry </option>
 					<option> Request a Proposal </option>
 					<option> Internship </option>
 					<option> Job Application </option>
 			</select>
+			<br>
 					<textarea>
 						Your message here.
 					</textarea>
+				<br>
 		      <button type="button" class="btna btn-primary btn-login"><b>SUBMIT</b></button>
-				
+		     </div>
+		     <div class="container-fluid">
+		      <div class="col-lg-5">
+			  	<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="385" id="gmap_canvas" src="https://maps.google.com/maps?q=136%20yakal%20st.%20makati%2Cphilippines&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.crocothemes.net"></a></div><style>.mapouter{text-align:right;height:385px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:385px;width:600px;}</style><small><a href="http://www.elegantthemes.com/affiliates/idevaffiliate.php?id=28532&url=42158&tid1=google3" rel="nofollow"></a></small></div></center></div>
 
-		  </div> 
-
+				</div>
+			</div>
 		  <center>
 
-		  	<div class="mapouter"><div class="gmap_canvas"><iframe width="585" height="385" id="gmap_canvas" src="https://maps.google.com/maps?q=136%20yakal%20st.%20makati%2Cphilippines&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.crocothemes.net"></a></div><style>.mapouter{text-align:right;height:385px;width:585px;}.gmap_canvas {overflow:hidden;background:none!important;height:385px;width:585px;}</style><small><a href="http://www.elegantthemes.com/affiliates/idevaffiliate.php?id=28532&url=42158&tid1=google3" rel="nofollow"></a></small></div></center>
+		  	
 		  </div>
 
 		</div>
