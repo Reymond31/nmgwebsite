e67d8b8cc61ee62ce5ad85b1d9064d030f6ed5b3
Reymond Samontena <reymond@nmgresources.ph>
2018-07-24 17:38:13 +0800

    Services

--- application/views/app/nmgwebsite/products.php
+++ application/views/app/nmgwebsite/products.php
@@ -3,17 +3,41 @@
 		<div class="row">
 			<div class="container">
 				<h1>PRODUCTS</h1>
-				<div class="col-lg-6 col-xs-6">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<img src="<?php echo base_url("assets/img/heffy.jpg")?>" class="heffy">
+				<div class="col-lg-6 col-xs-6 no-padding">
+					<div id="f1_container">
+						<div id="f1_card" class="shadow">
+  							<div class="front face">
+    							<img src="<?php echo base_url("assets/img/heffy.jpg")?>" class="product-site-left">
+  							</div>
+ 							 <div class="back face center">
+    							<p>This is nice for exposing more information about an image.</p>
+    							<p>Any content can go here.</p>
+  							</div>
 						</div>
 					</div>
 				</div>
-				<div class="col-lg-6 col-xs-6">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<img src="<?php echo base_url("assets/img/heffy.jpg")?>" class="heffy">
+				<div class="col-lg-6 col-xs-6 no-padding">
+    				<div class="heffy product-title-left">
+    					<p>Social mobile yearbook app which aims to revolutionize the traditional experience of having a yearbook.</p>
+    				</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="container">
+				<div class="col-lg-6 col-xs-6 no-padding">
+    				<img src="<?php echo base_url("assets/img/txtsupport.jpg")?>" class="product-site-right">
+				</div>
+				<div class="col-lg-6 col-xs-6 no-padding">
+					<div id="f2_container">
+						<div id="f2_card" class="shadow">
+  							<div class="front face">
+    							<img src="<?php echo base_url("assets/img/txtsupport.jpg")?>" class="product-title-right">
+  							</div>
+ 							 <div class="back face center">
+    							<p>This is nice for exposing more information about an image.</p>
+    							<p>Any content can go here.</p>
+  							</div>
 						</div>
 					</div>
 				</div>
@@ -21,17 +45,39 @@
 		</div>
 		<div class="row">
 			<div class="container">
-				<div class="col-lg-6 col-xs-6">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<img src="<?php echo base_url("assets/img/txtsupport.jpg")?>" class="txtsupport">
+				<div class="col-lg-6 col-xs-6 no-padding">
+					<div id="f1_container">
+						<div id="f1_card" class="shadow">
+  							<div class="front face">
+    							<img src="<?php echo base_url("assets/img/bes.jpg")?>" class="product-site-left">
+  							</div>
+ 							 <div class="back face center">
+    							<p>This is nice for exposing more information about an image.</p>
+    							<p>Any content can go here.</p>
+  							</div>
 						</div>
 					</div>
 				</div>
-				<div class="col-lg-6 col-xs-6">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<img src="<?php echo base_url("assets/img/txtsupport.jpg")?>" class="txtsupport">
+				<div class="col-lg-6 col-xs-6 no-padding">
+    				<img src="<?php echo base_url("assets/img/bes.jpg")?>" class="product-title-left">
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="container">
+				<div class="col-lg-6 col-xs-6 no-padding">
+    				<img src="<?php echo base_url("assets/img/cress.jpg")?>" class="product-site-right">
+				</div>
+				<div class="col-lg-6 col-xs-6 no-padding">
+					<div id="f2_container">
+						<div id="f2_card" class="shadow">
+  							<div class="front face">
+    							<img src="<?php echo base_url("assets/img/cress.jpg")?>" class="product-title-right">
+  							</div>
+ 							 <div class="back face center">
+    							<p>This is nice for exposing more information about an image.</p>
+    							<p>Any content can go here.</p>
+  							</div>
 						</div>
 					</div>
 				</div>
@@ -39,55 +85,21 @@
 		</div>
 		<div class="row">
 			<div class="container">
-				<div class="col-lg-6 col-xs-6">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<img src="<?php echo base_url("assets/img/bes.jpg")?>" class="bes">
+				<div class="col-lg-6 col-xs-6 no-padding">
+					<div id="f1_container">
+						<div id="f1_card" class="shadow">
+  							<div class="front face">
+    							<img src="<?php echo base_url("assets/img/pinoypayroll.jpg")?>" class="product-site-left">
+  							</div>
+ 							 <div class="back face center">
+    							<p>This is nice for exposing more information about an image.</p>
+    							<p>Any content can go here.</p>
+  							</div>
 						</div>
 					</div>
 				</div>
-				<div class="col-lg-6 col-xs-6">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<img src="<?php echo base_url("assets/img/bes.jpg")?>" class="bes">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="container">
-				<div class="col-lg-6 col-xs-6">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<img src="<?php echo base_url("assets/img/cress.jpg")?>" class="cress">
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-6 col-xs-6">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<img src="<?php echo base_url("assets/img/cress.jpg")?>" class="cress">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="container">
-				<div class="col-lg-6 col-xs-6">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<img src="<?php echo base_url("assets/img/pinoypayroll.jpg")?>" class="pinoypayroll">
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-6 col-xs-6">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<img src="<?php echo base_url("assets/img/pinoypayroll.jpg")?>" class="pinoypayroll">
-						</div>
-					</div>
+				<div class="col-lg-6 col-xs-6 no-padding">
+    				<img src="<?php echo base_url("assets/img/pinoypayroll.jpg")?>" class="product-title-left">
 				</div>
 			</div>
 		</div>
