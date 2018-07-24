6d2cde488aa1ccae7bdf34fdb3998b1e3d1ade32
Mae0201 <mae@nmgresources.ph>
2018-07-24 17:31:04 +0800

    grid the paragraph

--- application/views/app/nmgwebsite/aboutus.php
+++ application/views/app/nmgwebsite/aboutus.php
@@ -4,31 +4,12 @@
 		<div class="row">
 			<div class="col-lg-5">
 				<p class="big" data-aos="flip-down" data-aos-offset="200" data-aos-duration="2000" >We Are <br> NMG</p></div>
+
 			<div class="col-lg-7">
-        <p style="padding-top:100px; padding-left: 30px;font-size: 15px;letter-spacing: 2px;text-align: justify;color:#5a5b5b;float:right;font-family: Raleway, sans-serif;font-weight: normal;padding-bottom: 3px; color: black"> Founded in 2006, NMG I.T. Outsourcing Philippines is a Danish and Filipino owned product and software development outsourcing company that aims to deliver high quality and cost effective Internet products and services. It caps the production of dynamic and interactive websites and Internet/Intranet applications, as it also renders superior and inventive services from Information Systems maintenance to offshore web staffing.<p class="collapse" id="footwear" style=" padding-left: 30px;font-size: 15px; letter-spacing: 2px;text-align: justify;; color:#5a5b5b; float: right; font-family:Raleway, sans-serif; font-weight: normal; padding-bottom: 3px; color: black">On software development, NMG boasts its expertise on open source technologies like <b>LAMP</b> (Linux, Apache, MySQL and PHP) for its web-based applications development and <b>Asterisk</b> on PABX administration system development and VoIP installations. NMG uses the Waterfall software lifecycle methodology and <b>MVC</b> Architecture for quality assurance and rapid delivery. Upon investing of Filipino's visual arts talents, web design, logo making and other digital arts has also become NMG's expertise. NMG is confident of providing the best design solutions in static images, HTML and even animations. NMG's main goal is to promote interactivity, robust functionality as well as user-friendliness through its products and services that meet and satiate every client's expectations accordingly, with full accountability sans errs and delays.  That's why the company puts the clients at the top of its list, as it aims to meet the clients' utmost standards, helping them facilitate and develop their businesses through its maximized efforts. It also aims to take much heed on its target market: clients belonging to any kind of field who need an upgrade/refurbishing, automation and maintenance on their present systems...<p><a data-toggle="collapse" href="#footwear" aria-controls="footwear" style="float: right;"><b>Want to read more?</b></a></p></p></p>     
-	
-        <!-- jQuery library -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<!-- Bootstrap JS -->
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-<!-- Initialize Bootstrap functionality -->
-<script>
-// Initialize tooltip component
-        <!--// Initialize tooltip component -->
-        <script>
-        $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-        })
-
-        // Initialize popover component
-        $(function () {
-        $('[data-toggle="popover"]').popover()
-        })
-        </script>
-
-
-			</div>
-		</div>
+                                <div class="grid-container">
+                                      <div class="grid-item2"> Founded in 2006, NMG I.T. Outsourcing Philippines is a Danish and Filipino owned product and software development outsourcing company that aims to deliver high quality and cost effective Internet products and services. It caps the production of dynamic and interactive websites and Internet/Intranet applications, as it also renders superior and inventive services from Information Systems maintenance to offshore web staffing.</div> 
+                                 </div>
+                        </div>
+                </div>
         </div>
 </div>
