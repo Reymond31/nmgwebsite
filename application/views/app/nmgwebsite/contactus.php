086162755b32604a48cfb8aadafd0e8b2a4fdda1
Mae0201 <mae@nmgresources.ph>
2018-07-24 19:41:50 +0800

    textarea background color

--- application/views/app/nmgwebsite/contactus.php
+++ application/views/app/nmgwebsite/contactus.php
@@ -5,13 +5,13 @@
 			<h1> CONTACT US </h1>
 			<div class="col-lg-7">
 				<h2> Have a general question?</h2>
-				Fill up the form:<br>
+				<b>Fill up the form:</b><br><br>
 			
-			Name:
+			<b>Name:</b>
 			<br>
 			<input type="text" value="Your Name..." class="name"><br>
 			
-			Email:
+			<b>Email:</b>
 			<br>
 			<input type="email" value="Your Email..." class="email"><br>
 			<select class="option">
@@ -28,12 +28,14 @@
 				<br>
 		      <button type="button" class="btna btn-primary btn-login"><b>SUBMIT</b></button>
 		     </div>
+		     <div class="row">
 		     <div class="container-fluid">
 		      <div class="col-lg-5">
 			  	<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="385" id="gmap_canvas" src="https://maps.google.com/maps?q=136%20yakal%20st.%20makati%2Cphilippines&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.crocothemes.net"></a></div><style>.mapouter{text-align:right;height:385px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:385px;width:600px;}</style><small><a href="http://www.elegantthemes.com/affiliates/idevaffiliate.php?id=28532&url=42158&tid1=google3" rel="nofollow"></a></small></div></center></div>
 
 				</div>
 			</div>
+		</div>
 		  <center>
 
 		  	
