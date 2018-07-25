52773c4b731208cc6c98338e217480727b024ee3
Mae0201 <mae@nmgresources.ph>
2018-07-25 15:51:56 +0800

    contact us

--- application/views/app/nmgwebsite/contactus.php
+++ application/views/app/nmgwebsite/contactus.php
@@ -2,8 +2,8 @@
 	<div class="row">
 		<div class="container">
 			
-			<h1> CONTACT US </h1>
-			<div class="col-lg-7">
+			<h1>CONTACT US</h1>
+				<div class="col-lg-7">
 				<h2> Have a general question?</h2>
 				<b>Fill up the form:</b><br><br>
 			
@@ -22,16 +22,32 @@
 					<option> Job Application </option>
 			</select>
 			<br>
-					<textarea>
-						Your message here.
-					</textarea>
+					<textarea>Your message here......</textarea>
 				<br>
-		      <button type="button" class="btna btn-primary btn-login"><b>SUBMIT</b></button>
+		      <button type="button" class="button" style="vertical-align:middle"><span><b>SUBMIT</b></span></button>
 		     </div>
 		     <div class="row">
 		     <div class="container-fluid">
 		      <div class="col-lg-5">
 			  	<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="385" id="gmap_canvas" src="https://maps.google.com/maps?q=136%20yakal%20st.%20makati%2Cphilippines&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.crocothemes.net"></a></div><style>.mapouter{text-align:right;height:385px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:385px;width:600px;}</style><small><a href="http://www.elegantthemes.com/affiliates/idevaffiliate.php?id=28532&url=42158&tid1=google3" rel="nofollow"></a></small></div></center></div>
+			  	
+			  	<script>
+			  		var iconBase = 'https://maps.google.com/maps?q=136%20yakal%20st.%20makati%2Cphilippines&t=&z=17&ie=UTF8&iwloc=&output=embed';
+        			var icons = { NMG: 
+	         			{
+	            		icon: iconBase + 'logo.png'
+	          			},
+			        };
+			        
+     				features.forEach(function(feature) {
+          			var marker = new google.maps.Marker
+          				({
+            				position: feature.position,
+            				icon: icons[feature.type].icon,
+            				map: map
+          				});
+        			});
+    			</script>
 
 				</div>
 			</div>
