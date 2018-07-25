a39a0244f8dc100a70dce45b60bcc6ce841a5c25
Mae0201 <mae@nmgresources.ph>
2018-07-25 18:25:59 +0800

    contact us

--- application/views/app/nmgwebsite/contactus.php
+++ application/views/app/nmgwebsite/contactus.php
@@ -9,11 +9,11 @@
 			
 			<b>Name:</b>
 			<br>
-			<input type="text" value="Your Name..." class="name"><br>
+			<input type="text" placeholder="Your name..." class="name"><br>
 			
 			<b>Email:</b>
 			<br>
-			<input type="email" value="Your Email..." class="email"><br>
+			<input type="email" placeholder="Your email..." class="email"><br>
 			<select class="option">
 				<option>Purpose</option>
 					<option> General Inquiry </option>
@@ -22,15 +22,14 @@
 					<option> Job Application </option>
 			</select>
 			<br>
-					<textarea>Your message here......</textarea>
+					<textarea placeholder="Enter your message here!!..."></textarea>
 				<br>
 		      <button type="button" class="button" style="vertical-align:middle"><span><b>SUBMIT</b></span></button>
 		     </div>
 		     <div class="row">
 		     <div class="container-fluid">
 		      <div class="col-lg-5">
-			  	<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="385" id="gmap_canvas" src="https://maps.google.com/maps?q=136%20yakal%20st.%20makati%2Cphilippines&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.crocothemes.net"></a></div><style>.mapouter{text-align:right;height:385px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:385px;width:600px;}</style><small><a href="http://www.elegantthemes.com/affiliates/idevaffiliate.php?id=28532&url=42158&tid1=google3" rel="nofollow"></a></small></div></center></div>
-			  	
+
 			  	<script>
 			  		var iconBase = 'https://maps.google.com/maps?q=136%20yakal%20st.%20makati%2Cphilippines&t=&z=17&ie=UTF8&iwloc=&output=embed';
         			var icons = { NMG: 
@@ -49,16 +48,12 @@
         			});
     			</script>
 
+			  	<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="385" id="gmap_canvas" src="https://maps.google.com/maps?q=136%20yakal%20st.%20makati%2Cphilippines&t=&z=17&ie=UTF8&iwloc=&output=embed " frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.crocothemes.net"></a></div><style>.mapouter{text-align:right;height:385px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:385px;width:600px;}</style><small><a href="http://www.elegantthemes.com/affiliates/idevaffiliate.php?id=28532&url=42158&tid1=google3" rel="nofollow"></a></small></div></center></div>
+			  
 				</div>
 			</div>
-		</div>
-		  <center>
-
-		  	
-		  </div>
 
 		</div>
 	</div>
-</center></div>
 </div>
 
