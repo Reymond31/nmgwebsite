ab4e8c6a429c7d200172ddaad1726f05bbb51a42
Ara <ara@nmgresources.ph>
2018-07-26 14:34:55 +0800

    CONTACT US

--- application/views/app/nmgwebsite/contactus.php
+++ application/views/app/nmgwebsite/contactus.php
@@ -1,25 +1,22 @@
 <div id="contactus">
 	<div class="row">
 		<div class="container">
-			<div class="contact-us">
-				<h1>CONTACT US</h1>
-			</div>
-			<div class="col-lg-7">
-				<h2> Have a general question?</h2>
-					<b>Fill up the form:</b><br>
-					<b>Name:</b><br>
-						<input type="text" placeholder="Your name..." class="name"><br>
-					<b>Email:</b><br>
-						<input type="email" placeholder="Your email..." class="email"><br>
+			<div class="col-lg-7">				
+				<h2 class="head"> Have a general question?</h2>
+				<p class="con">CONTACT US!</p>
+					<span class="labelscontact">Name:</span>
+						<input type="text" placeholder="Your name" class="name">
+					<span class="labelscontact"><br>Email:</span>
+						<input type="email" placeholder="thisisyou@example.com" class="email">
 				<select class="option">
 					<option>Purpose</option>
 					<option> General Inquiry </option>
 					<option> Request a Proposal </option>
 					<option> Internship </option>
 					<option> Job Application </option>
-				</select><br>
-				<textarea placeholder="Enter your message here!!..."></textarea><br>
-		     	<button type="button" class="button" style="vertical-align:middle"><span><b>SUBMIT</b></span></button>
+				</select>
+				<textarea placeholder="Enter message here."></textarea>
+		     	<button type="button" class="button" style="vertical-align:"><span>SUBMIT</span></button>
 		     </div>
 		      <div class="col-lg-5">
 				<div class="mapouter">
