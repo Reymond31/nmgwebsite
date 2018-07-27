e680a1f23da2b9b92ecfefcb8443d2b9f3e2f542
Reymond Samontena <reymond@nmgresources.ph>
2018-07-27 16:42:15 +0800

    products

--- application/views/app/header.php
+++ application/views/app/header.php
@@ -19,13 +19,13 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/products.css")?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/contactus.css")?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/landingpage.css")?>">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	<script src="<?php echo base_url("assets/js/aos.js")?>"></script>
 	<script src="<?php echo base_url("assets/js/fontawesome.js")?>"></script>
 	<script src="<?php echo base_url("assets/js/fontawesome.min.js")?>"></script>
 	<script src="<?php echo base_url("assets/js/bootstrap.js")?>"></script>
-	<script src="<?php echo base_url("assets/js/bootstrap.min.js")?>"></script>
+	<script src="<?php echo base_url("assets/js/bootstrap.min.js")?>"></script>>
 	<script src="<?php echo base_url("assets/js/jquery-3.3.1.min.js")?>"></script>
+	<script src="<?php echo base_url("assets/js/jquery-1.9.1.js")?>"></script>
 	<script src="<?php echo base_url("assets/js/jquery-1.6.2.js")?>"></script>
 	<style>
 	#more {display: none;}
--- application/views/app/nmgwebsite/products.php
+++ application/views/app/nmgwebsite/products.php
@@ -17,6 +17,28 @@
   							</div>
  							<div class="back face center">
     							<p class="z">Established in 2014, Heffy is a complete platform used to bring the hard-bound yearbook to the digital age. Schools and universities subscribe to Heffy, and manage everything from student registration to pictorial scheduling. Students are able to download the free mobile app, log in and view their images.
+    							<button type="text" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     							</p>
   							</div>
 						</div>
