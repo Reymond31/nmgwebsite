d4edbdfa882375de979cd830e6f3d092c615d04d
Mae0201 <mae@nmgresources.ph>
2018-07-31 13:47:31 +0800

    featured products

--- application/views/app/nmgwebsite/products.php
+++ application/views/app/nmgwebsite/products.php
@@ -69,10 +69,45 @@
   							</div>
  							 <div class="back face center">
     							<p class="z">Grand Prize winner of the 2015 Chikka API Challenge, TxtSupport is a web-based support platform using SMS to communicate with users. The portal includes CRM functionality to help businesses manage and grow their users. The platform’s standout feature is allowing SMS customers to communicate with business at no cost. TxtSupport has a growing base of users for the platform.</p>
+                    <a href="#" data-toggle="modal" data-target=".bs-example-modal-lg">Take a tour?</a>
   							</div>
 						</div>
 					</div>
 				</div>
+          <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+              <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="item active">
+                    <img class="img-responsive" src="<?php echo base_url("textsupport.PNG")?>" alt="heffy-1">
+                    <div class="carousel-caption">
+                      One Image
+                    </div>
+                </div>
+                <div class="item">
+                    <img class="img-responsive" src="<?php echo base_url("assets/img/heffy-2.jpg")?>" alt="heffy-2">
+                    <div class="carousel-caption">
+                      Another Image
+                    </div>
+                </div>
+                <div class="item">
+                    <img class="img-responsive" src="<?php echo base_url("assets/img/heffy-3.jpg")?>" alt="heffy-3">
+                    <div class="carousel-caption">
+                      Another Image
+                    </div>
+                </div>
+              </div>
+              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <i class="icon-chevron-left"></i>
+              </a>
+              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <i class="icon-chevron-right"></i>
+            </a>
+          </div>
+          </div>
+        </div>
+    </div>
 				<div class="col-lg-6 col-xs-6 no-padding">
 					<div class="textsupport product-title-left">
     					<p class="x"> TxtSupport </p>
@@ -97,12 +132,48 @@
   							</div>
  							 <div class="back face center">
     							<p class="z">Customer engagement with brands are ever-increasing with Facebook. Conducting any scale of customer support using Facebook Messenger involves manpower and training. NMG created a powerful and simple solution to use Chatbot which guides customers over chat. The range of functionality extend from quick walk through to complex E-Commerce processes, all managed within an integrated platform.</p>
+                  <a href="#" data-toggle="modal" data-target=".bs-example-modal-lg">Take a tour?</a>
+
   							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
+    <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+              <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="item active">
+                    <img class="img-responsive" src="<?php echo base_url("textsupport.PNG")?>" alt="heffy-1">
+                    <div class="carousel-caption">
+                      One Image
+                    </div>
+                </div>
+                <div class="item">
+                    <img class="img-responsive" src="<?php echo base_url("assets/img/heffy-2.jpg")?>" alt="heffy-2">
+                    <div class="carousel-caption">
+                      Another Image
+                    </div>
+                </div>
+                <div class="item">
+                    <img class="img-responsive" src="<?php echo base_url("assets/img/heffy-3.jpg")?>" alt="heffy-3">
+                    <div class="carousel-caption">
+                      Another Image
+                    </div>
+                </div>
+              </div>
+              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <i class="icon-chevron-left"></i>
+              </a>
+              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <i class="icon-chevron-right"></i>
+            </a>
+          </div>
+          </div>
+        </div>
+    </div>
 		<div class="row">
 			<div class="container">
 				<div class="col-lg-6 col-xs-6 no-padding">
@@ -113,6 +184,7 @@
   							</div>
  							 <div class="back face center">
     							<p class="z">Upon the request of the City of Manila, CReSS was developed to help streamline and automate the document management tasks of the City Hall. With over 30 million digital records and growing, CReSS has transformed the operations of City Hall. As a product of NMG, CReSS is currently being marketed to other government agencies. </p>
+
   							</div>
 						</div>
 					</div>
@@ -141,10 +213,45 @@
   							</div>
  							 <div class="back face center">
     							<p class="z">Marketing promotions have grown in the past decade, often exceeding the capabilities of existing point-of-sales devices. POS Partner works with any standard Windows-based POS, and automatically allows marketing solutions without any changes to the existing POS. </p>
+                  <a href="#" data-toggle="modal" data-target=".bs-example-modal-lg">Take a tour?</a>
   							</div>
 						</div>
 					</div>
     			</div>
+           <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+              <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="item active">
+                    <img class="img-responsive" src="<?php echo base_url("textsupport.PNG")?>" alt="heffy-1">
+                    <div class="carousel-caption">
+                      One Image
+                    </div>
+                </div>
+                <div class="item">
+                    <img class="img-responsive" src="<?php echo base_url("assets/img/heffy-2.jpg")?>" alt="heffy-2">
+                    <div class="carousel-caption">
+                      Another Image
+                    </div>
+                </div>
+                <div class="item">
+                    <img class="img-responsive" src="<?php echo base_url("assets/img/heffy-3.jpg")?>" alt="heffy-3">
+                    <div class="carousel-caption">
+                      Another Image
+                    </div>
+                </div>
+              </div>
+              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <i class="icon-chevron-left"></i>
+              </a>
+              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <i class="icon-chevron-right"></i>
+            </a>
+          </div>
+          </div>
+        </div>
+    </div>
 			</div>
 		</div>
 	</div>
