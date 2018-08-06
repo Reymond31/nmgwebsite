f819e5683b378578c43e314b584a4c41d6c22e68
Mae0201 <mae@nmgresources.ph>
2018-08-06 16:09:48 +0800

    animation

--- application/views/app/nmgwebsite/services.php
+++ application/views/app/nmgwebsite/services.php
@@ -2,47 +2,69 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-5">
+				 <div class="item" data-aos="fade-down">
 				<p class="head">WHAT WE</p><br>
 				<p class="heads"> DO?</p>
 			</div>
+			</div>
 			<div class="col-lg-3">
+			<div class="item" data-aos="fade-down">
 				<img src="assets/img/monitors.png" class="laptop">
 			</div>
+			</div>
 			<div class="col-lg-4">
+				 <div class="item" data-aos="fade-down">
 				<p class="laptop1">Desktop Application Development</p>
 				<p class="laptop2">Custom application development using Java/.Net. Applications range from client-server monitoring systems to full CRM suites.</p>
 			</div>
+			</div>
 		</div>
 		<div class="row">
 			<div class="col-lg-2">
+				<div class="item" data-aos="fade-down">
 				<img src="assets/img/penfinals.png" class="pen1">
 			</div>
+			</div>
 			<div class="col-lg-4">
+				 <div class="item" data-aos="fade-down">
 				<p class="pen2">Web App Design and Development</p>	
 				<p class="pen3">PHP/nodeJS custom development, including relevant database solutions. Applications are built using modern frameworks and industry leading scalability.</p>
 			</div>
+			</div>
 			<div class="col-lg-2">
+				<div class="item" data-aos="fade-down">
 				<img src="assets/img/ph.png" class="phone">
 			</div>
+			</div>
 			<div class="col-lg-4">
+				 <div class="item" data-aos="fade-down">
 				<p class="mobile1">Mobile Application for Android and iOS</p>
 				<p class="mobile2">Designing and development of both native and cross platform mobile applications. Applications backends are also designed with web-based management portals aligned to customer requirements.</p>
+			</div>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-lg-2">	
+				<div class="item" data-aos="fade-down">
 				<img src="assets/img/staffi.png" class="staff">
 			</div>
+			</div>
 			<div class="col-lg-4">	
+				 <div class="item" data-aos="fade-down">
 				<p class="staff1">Staff Augmentation</p>
  				<p class="staff2">Providing experienced developers and database administrators to local industries, managed for specific requirements of the business.</p>
  			</div>
+ 			</div>
 			<div class="col-lg-2">	
+				<div class="item" data-aos="fade-down">
 				<img src="assets/img/tec.png" class="tech">
 			</div>
+			</div>
 			<div class="col-lg-4">	
+				 <div class="item" data-aos="fade-down">
 				<p class="tech1">Technology & Management Consulting</p>
  				<p class="tech2">Specialist consulting services focused on requirements analysis, system design and project management, delivered to enterprise-level clients.</p>
+ 			</div>
 			</div>
 		</div>
 	</div>
