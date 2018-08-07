6dba2e8bb3eb6bd5cfac6e986f528d96ca35c8b2
Reymond Samontena <reymond@nmgresources.ph>
2018-08-07 15:07:17 +0800

    About Us

--- application/views/app/nmgwebsite/aboutus.php
+++ application/views/app/nmgwebsite/aboutus.php
@@ -1,74 +1,74 @@
 <div id="aboutus">
-  <section class="background">
-    <div class="container-fluid">
+  <div class="container-fluid">
+    <section class="background">
       <div class="container">
         <div class="row">
           <div class="col-lg-5">
             <div class="item" data-aos="zoom-in">
-            <p class="big" >WHO WE</p><br>
-            <p class="small">ARE?</p>
-          </div>
-          </div>
-            <div class="col-lg-7">
-              <div class="grid-item2"> 
-               <div class="item" data-aos="fade-left">
-              Founded in 2006, NMGResources Inc. Philippines  started as a custom software development company. In the last decade. NMG has provided targeted software solutions across multiple industries including government and enterprise entities, both local, and international. NMG’s services encompass application development for desktop, web, mobile, and server solutions. The company has also established a growing in-house portfolio of products developed for the end-user market, providing consulting services to multinational firms ranging from technology expertise to enterprise-level project management.
+              <p class="big" >WHO WE</p><br>
+              <p class="small">ARE?</p>
             </div>
+          </div>
+          <div class="col-lg-7">
+            <div class="grid-item2"> 
+              <div class="item" data-aos="fade-left">
+                Founded in 2006, NMGResources Inc. Philippines  started as a custom software development company. In the last decade. NMG has provided targeted software solutions across multiple industries including government and enterprise entities, both local, and international. NMG’s services encompass application development for desktop, web, mobile, and server solutions. The company has also established a growing in-house portfolio of products developed for the end-user market, providing consulting services to multinational firms ranging from technology expertise to enterprise-level project management.
               </div>
             </div>
-            </div>
+          </div>
         </div>
+      </div>
     </section>
-      <div class="container overlays">
-        <img src="assets/img/aboutus3.png" id="stroke">
+    <div class="container overlays">
+      <img src="assets/img/aboutus3.png" id="stroke">
       <div class="row">
         <div class="col-lg-12">
           <div class="item" data-aos="zoom-in">
-           <p class="clients">OUR CLIENTS</p>
-         </div>
+            <p class="clients">OUR CLIENTS</p>
+          </div>
         </div>
       </div>
       <div class="row">
         <div class="col-lg-4">
           <div class="item" data-aos="fade-down">
-          <img src="<?php echo base_url("assets/img/jollibee.png")?>" class="client-logo">
-          <p class="captions"><b>Jolibee Foods Corp.</b></p>
-        </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="item" data-aos="fade-down">
-          <img src="<?php echo base_url("assets/img/DOLE.png")?>" class="client-logo">
-          <p class="captions"><b>Department of Labor and Employment - Philippines</b></p>
-        </div>
+            <img src="<?php echo base_url("assets/img/jollibee.png")?>" class="client-logo">
+            <p class="captions"><b>Jolibee Foods Corp.</b></p>
+          </div>
         </div>
         <div class="col-lg-4">
           <div class="item" data-aos="fade-down">
-          <img src="<?php echo base_url("assets/img/MANILA.png")?>" class="client-logo">
-          <p class="captions"><b>City of Manila,<br>Philippines</b></p>
+            <img src="<?php echo base_url("assets/img/DOLE.png")?>" class="client-logo">
+            <p class="captions"><b>Department of Labor and Employment - Philippines</b></p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="item" data-aos="fade-down">
+            <img src="<?php echo base_url("assets/img/MANILA.png")?>" class="client-logo">
+            <p class="captions"><b>City of Manila,<br>Philippines</b></p>
+          </div>
         </div>
       </div>
       <div class="row">
-       <div class="col-lg-4">        
+        <div class="col-lg-4">        
           <div class="item" data-aos="fade-down">
-          <img src="<?php echo base_url("assets/img/UNITEDNATIONS.png")?>" class="client-logo undp">
-          <p class="captions captions-other"><b>United Nations Development Programme</b></p>
-        </div>
+            <img src="<?php echo base_url("assets/img/UNITEDNATIONS.png")?>" class="client-logo undp">
+            <p class="captions captions-other"><b>United Nations Development Programme</b></p>
+          </div>
         </div>
         <div class="col-lg-4">          
           <div class="item" data-aos="fade-down">
-          <img src="<?php echo base_url("assets/img/ILO.png")?>" class="client-logo ilo">
-          <p class="captions captions-other"><b>International Labour Organization</b></p>
-        </div>
+            <img src="<?php echo base_url("assets/img/ILO.png")?>" class="client-logo ilo">
+            <p class="captions captions-other"><b>International Labour Organization</b></p>
+          </div>
         </div>
         <div class="col-lg-4">          
           <div class="item" data-aos="fade-down">
-          <img src="<?php echo base_url("assets/img/trendmicro.png")?>" class="client-logo microtrend">
-          <p class="captions captions-other"><b>Trend Micro</b></p>
+            <img src="<?php echo base_url("assets/img/trendmicro.png")?>" class="client-logo microtrend">
+            <p class="captions captions-other"><b>Trend Micro</b></p>
+          </div>
         </div>
-        </div>
       </div>
-        <div id="svc"></div> 
-      </div>
-      </div>
+    </div>
   </div>
+  <div id="svc"></div>
 </div>
