28f9ea18c3d08c5ffaadea11f3b7345d6ec3907c
Ara <ara@nmgresources.ph>
2018-08-09 19:07:27 +0800

    changes

--- application/views/app/nmgwebsite/products.php
+++ application/views/app/nmgwebsite/products.php
@@ -10,36 +10,36 @@
     <div class="carousel-inner" role="listbox">
       <div class="item active">
        <img class="slides" src="<?php echo base_url("assets/img/heffy.jpg")?>" alt="heffy">
-          <div class="carousels"> HEFFY.CO</div>
+          <div class="carousels"><br> HEFFY.CO</div>
           <div class="carousel-caption">
            <p>Established in 2014, Heffy is a complete platform used to bring the hard-bound yearbook to the digital age. Schools and universities subscribe to Heffy, and manage everything from student registration to pictorial scheduling. Students are able to download the free mobile app, log in and view their images.</p>
           </div>
       </div>
       <div class="item">
         <img class="slides" src="<?php echo base_url("assets/img/txtsupport.jpg")?>" alt="txtsupport" >
-          <div class="carousels">  TXT<br>SUPPORT</div>
+          <div class="carousels"> TXT<br>SUPPORT</div>
           <div class="carousel-caption">
            <p> Grand Prize winner of the 2015 Chikka API Challenge, TxtSupport is a web-based support platform using SMS to communicate with users. The portal includes CRM functionality to help businesses manage and grow their users. The platform’s standout feature is allowing SMS customers to communicate with business at no cost. TxtSupport has a growing base of users for the platform.</p>
           </div>
       </div>
       <div class="item">
         <img class="slides" src="<?php echo base_url("assets/img/bes.jpg")?>" alt="bes" >
-          <div class="carousels">  BES <br>CHATBOT</div>
+          <div class="carousels">  <br>BES <br>CHATBOT</div>
           <div class="carousel-caption">
           Customer engagement with brands are ever-increasing with Facebook. Conducting any scale of customer support using Facebook Messenger involves manpower and training. NMG created a powerful and simple solution to use Chatbot which guides customers over chat. The range of functionality extend from quick walk through to complex E-Commerce processes, all managed within an integrated platform.
           </div>
       </div>
       <div class="item">
         <img class="slides" src="<?php echo base_url("assets/img/cress.jpg")?>" alt="cress" >
-          <div class="carousels">  CRESS</div>
+          <div class="carousels"> <br> CRESS</div>
           <div class="carousel-caption">
             Upon the request of the City of Manila, CReSS was developed to help streamline and automate the document management tasks of the City Hall. With over 30 million digital records and growing, CReSS has transformed the operations of City Hall. As a product of NMG, CReSS is currently being marketed to other government agencies.
           </div>
       </div>
       <div class="item">
         <img class="slides" src="<?php echo base_url("assets/img/pinoypayroll.jpg")?>" alt="pinoypayroll" >
-          <div class="carousels">  PINOY<br>PAYROLL</div>
-          <div class="carousel-caption">
+          <div class="carousels_pinoypayroll"> PINOY<br>PAYROLL</div>
+          <div class="carousels-caption">
             Marketing promotions have grown in the past decade, often exceeding the capabilities of existing point-of-sales devices. POS Partner works with any standard Windows-based POS, and automatically allows marketing solutions without any changes to the existing POS.
           </div>
       </div>
